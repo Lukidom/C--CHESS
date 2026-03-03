617f5cba38315f329a4c66c70669649e049706b0
lukidom <swingfighter>
2026-03-03 15:41:00 -0700

    Update 1.1: After reasearch a simple format is built out.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello world\n";
-    return 0;
-}
+
+
+//  Enums
+
+//  Piece class
+
+
+class board {
+    public: 
+        piece board[8][8];
+
+        void printBoard();
+        Bool movePiece(int sr, int sc, int er, int ec);
+};
+
+main()
+
